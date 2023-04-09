8e976fdd6c4f57c47ac7768211edfad9ec5125af
betology <betology@gmail.com>
2023-04-09 12:46:36 -0600

    removed return statement

--- book/book.go
+++ book/book.go
@@ -5,16 +5,16 @@ import (
 )
 
 func GetBooks(c *fiber.Ctx) error {
-	return c.SendString("All Books")
+	c.SendString("All Books")
 }
 
 func GetBook(c *fiber.Ctx) error {
-	return c.SendString("A Single Book")
+	c.SendString("A Single Book")
 }
 
 func NewBook(c *fiber.Ctx) error{
-	return c.SendString("Adds a new Book")
+	c.SendString("Adds a new Book")
 }
 func DeleteBook(c *fiber.Ctx) error{
-	return c.SendString("Deletes a Book")
+	 c.SendString("Deletes a Book")
 }
