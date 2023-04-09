c39a9e41f54506779f14d3dd9393e9773cdd3748
betology <betology@gmail.com>
2023-04-09 12:36:56 -0600

    fixed book.go

--- book/book.go
+++ book/book.go
@@ -5,16 +5,16 @@ import (
 )
 
 func GetBooks(c *fiber.Ctx) {
-	s.SendString("All Books")
+	c.SendString("All Books")
 }
 
 func GetBook(c *fiber.Ctx) {
-	s.SendString("A Single Book")
+	c.SendString("A Single Book")
 }
 
 func NewBook(c *fiber.Ctx) {
-	s.SendString("Adds a new Book")
+	c.SendString("Adds a new Book")
 }
 func DeleteBook(c *fiber.Ctx) {
-	s.SendString("Deletes a Book")
+	c.SendString("Deletes a Book")
 }
