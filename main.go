195caa3e0d1cd4baa72dc2778b9fbffe74a4f3c7
betology <betology@gmail.com>
2023-04-09 12:31:39 -0600

    still working on routes

--- main.go
+++ main.go
@@ -11,11 +11,15 @@ func helloWorld(c *fiber.Ctx) error {
 
 func setupRoute(app *fiber.App) {
 	app.Get("/api/v1/book", book.GetBooks)
+	app.Get("/api/v1/book/:id", book.GetBook)
+	app.Post("/api/v1/book", book.NewBook)
+	app.Delete("/api/v1/book/:id", book.DeleteBook)
+}
 
 func main() {
     app := fiber.New()
 
-    app.Get("/", helloWorld)
+    setupRoutes(app)
 
     app.Listen(":3000")
 }
